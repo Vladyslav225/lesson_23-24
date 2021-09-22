ce8a9412c8819f5c07cfab303cd255f5697c3041
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-23 01:34:44 +0300

    .

--- the_highest_value.py
+++ the_highest_value.py
@@ -1,12 +1,6 @@
 # Найдите три ключа с самыми высокими значениями в словаре 
 # _dict = {'1key':123, '2key':9785, '3key': 560, '4key':234, '5key': 3451, '6key': 11111}
 
-# sorted_dict = sorted(_dict, reverse=False, key=_dict.get)
-# print(sorted_dict)
-
-# for n in sorted_dict[-3:]:
-#      print(n)
-
 def search_max_keys(_dict):
 
      sorted_dict = sorted(_dict, reverse=False, key=_dict.get)
