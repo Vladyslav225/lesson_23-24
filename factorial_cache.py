c971bc1dedd8078aa081ea8079fc96bc0d48efcd
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-10-11 23:29:45 +0300

    .

--- factorial_cache.py
+++ factorial_cache.py
@@ -5,21 +5,30 @@ dict_ = {}
 
 def factorial():
 
-     while True:
+     for _ in range(10):
+          
+          try:
 
-          factorial_number = int(input('Input Number: '))
+               factorial_number = int(input('Input Number: '))
 
-          if factorial_number in dict_:
-               print(f'Cache {dict_[factorial_number]}')
+               if factorial_number in dict_:
+                    print(f'Cache {dict_[factorial_number]}')
 
-          elif factorial_number not in dict_:
+               elif factorial_number not in dict_:
 
-               factorial = 1
-               for i in range(1, factorial_number+1):
-                    factorial *= i
-               print(f'Fuctorial number {factorial_number} "=" {factorial}')
+                    factorial = 1
 
-               dict_[factorial_number] = factorial
+                    for i in range(1, factorial_number+1):
+                         factorial *= i
+
+                    print(f'Fuctorial number {factorial_number} = {factorial}')
+
+                    dict_[factorial_number] = factorial
+
+          except Exception as ex:
+               print(ex)
+               exit
+
 
 factorial()
 
